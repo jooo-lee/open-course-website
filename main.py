87dbbfa96f8f9b8b720495a208c0e3475cc9e934
Joseph Lee <josephlee3004@gmail.com>
2022-11-11 18:00:28 -0500

    Comment out time.sleeps

--- main.py
+++ main.py
@@ -11,26 +11,26 @@ s = Service("./chromedriver")
 driver = webdriver.Chrome(service=s)
 
 driver.get(url)
-time.sleep(1)
+# time.sleep(1)
 
 login_btn_1 = driver.find_element(By.XPATH, "/html/body/table[3]/tbody/tr/td[1]/table/tbody/tr[3]/td")
 login_btn_1.click()
-time.sleep(1)
+# time.sleep(1)
 
 sno_form = driver.find_element(By.NAME, "sno")
 sno_form.send_keys(sno)
-time.sleep(1)
+# time.sleep(1)
 
 login_btn_2 = driver.find_element(By.NAME, "login")
 login_btn_2.click()
-time.sleep(1)
+# time.sleep(1)
 
 password_form = driver.find_element(By.NAME, "p1")
 password_form.send_keys(password)
-time.sleep(1)
+# time.sleep(1)
 
 login_btn_3 = driver.find_element(By.NAME, "login")
 login_btn_3.click()
-time.sleep(2)
+# time.sleep(2)
 
 # driver.quit()
